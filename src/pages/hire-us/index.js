526de9301ea3e4632ff46f01d8e8df46befbd1d6
atrain42 <maxheimer4211@gmail.com>
2023-05-17 20:53:36 -0400

    hire us page updates

--- src/pages/hire-us/index.js
+++ src/pages/hire-us/index.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import Link from 'next/link'
 import Contact from '@/layout/Contact'
 import Image from 'next/image'
 import brand11 from '../../../public/images/brand11.webp'
+import branding12 from '../../../public/images/branding12.webp'
 import branding2 from '../../../public/images/branding-2.webp'
 import branding69 from '../../../public/images/branding69.webp'
 import Main from '@/components/Accordion/Main'
 import Form from '@/components/Form/Form'
 
 export default function index() {
+  // const ref = useRef(null)
+  const scrollToForm = () => ref.current?.scrollIntoView({ behavior: 'smooth' })
   return (
     <div className='px-8'>
-      <div className='hire-landing flex flex-col items-center justify-center'>
+      <header className='flex items-center mv:px-8 mv:flex-col-reverse mv:justify-center mv:pt-8 md:flex-row md:justify-evenly md:pt-20'>
+        <div className='mv:mr-0 mv:w-full md:mr-4 md:w-100 text-center'>
+          <p className='text-theme font-semibold mb-4'>FOR YOU</p>
+          <h1 className='text-5xl'>
+            <strong>Brand building</strong>
+          </h1>
+          <p className='mt-5 text-lg w-full'>
+            Are you looking to take your design beyond the free illustrations we
+            offer? Then take a journey with us into the Forbidden Forest and
+            unlock your design&#39;s true potential.
+          </p>
+          {/* <p>
+          Today, so many websites are using the same style of vector images. We
+          are here to disrupt that trend.
+        </p> */}
+          <div className='grid grid-cols-1 mt-8 gap-x-9 gap-y-9 font-button place-items-center text-md tracking-wider'>
+            <button
+              className='shadow-button w-44 mx-auto rounded font-poppins font-normal py-4 bg-theme text-white hover:scale-102 transition-all ease-in duration-50'
+              onClick={scrollToForm}
+            >
+              Get started
+            </button>
+          </div>
+        </div>
+        <div className='mv:ml-0 mv:h-full mv:w-full md:ml-4 md:h-200 md:w-auto mv:mb-12 md:mb-0'>
+          <Image
+            className='mv:h-5/6 mv:w-5/6 md:h-full md:w-auto mv:mx-auto'
+            src={branding12}
+            alt='platform'
+            priority
+          />
+        </div>
+      </header>
+      {/* <div className='hire-landing flex flex-col items-center justify-center'>
         <p className='text-theme font-semibold mb-4'>FOR YOU</p>
         <h1 className='text-5xl font-semibold mb-3'>Brand Building</h1>
         <p className='text-xl mb-3 font-medium'>Let us help build your UI/UX</p>
@@ -24,7 +61,7 @@ export default function index() {
           <div className='bg-offwhite h-12 w-12 rounded-full mx-4'></div>
           <div className='bg-secondary h-12 w-12 rounded-full'></div>
         </div>
-      </div>
+      </div> */}
       <div className='flex flex-col justify-center items-center mv:px-8 md:px-0 mb-12 mt-72'>
         <h1 className='text-4xl text-center mb-4 w-100'>
           Need custom illustrations? We got you covered.
@@ -66,8 +103,9 @@ export default function index() {
           Why choose Vector Owl?
         </h1>
         <p className='text-center mv:w-full sm:w-500'>
-          We will work with you at every step to ensure that the illustrations
-          we create fit your project&#39;s vision.
+          We create graphic illustrations that will leave a lasting impression
+          on your users. We will work with you at every step to ensure that the
+          illustrations we create fit your project&#39;s vision.
         </p>
         <div className='flex items-center justify-around mt-16 mv:flex-col-reverse md:flex-row'>
           <Image
@@ -85,8 +123,8 @@ export default function index() {
           Don&#39;t wait any longer. Fill out the form below and we will review
           your request and respond via email.
         </p>
-        <Form />
       </div>
+      <Form />
     </div>
   )
 }
