1f15b3ce53fda48bd7466c21c85cd9c89da8de87
atrain42 <maxheimer4211@gmail.com>
2023-05-29 13:32:04 -0400

    added twitter meta data

--- src/app/head.js
+++ src/app/head.js
@@ -1,8 +1,21 @@
+import twitterCard from '../../public/images/twitter-card.png'
+
 export default function Head() {
   return (
     <>
       <title>Free Vector Illustrations for Your Website</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@_PixelPrism' />
+      <meta name='twitter:title' content='Unique Vector Illustrations' />
+      <meta
+        name='twitter:description'
+        content='Enjoy our collections of free vector illustrations'
+      />
+      <meta
+        name='twitter:image'
+        content={twitterCard}
+      />
       <meta
         name='description'
         content='Explore a variety of free vector illustrations to enhance your website. Our vector art is available for download in WebP format. Premium illustrations available.'
