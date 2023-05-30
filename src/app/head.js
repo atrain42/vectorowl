ca1e988c3ba07eccf96c5b5aadce2210e47ea829
atrain42 <maxheimer4211@gmail.com>
2023-05-30 18:32:45 -0400

    4.0

--- src/app/head.js
+++ src/app/head.js
@@ -1,11 +1,14 @@
-import image from '../../public/images/twitter-card.png'
+import image from './twitter-card.png'
 
 export default function Head() {
   return (
     <>
       <title>Free Vector Illustrations for Your Website</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta property='og:image' content={`https://www.vectorowl.com/${image}`} />
+      <meta
+        property='og:image'
+        content={`https://www.vectorowl.com/${image}`}
+      />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:site' content='@_PixelPrism' />
       <meta name='twitter:title' content='Unique Vector Illustrations' />
@@ -13,7 +16,10 @@ export default function Head() {
         name='twitter:description'
         content='Enjoy our collections of free vector illustrations'
       />
-      <meta name='twitter:image' content={image} />
+      <meta
+        name='twitter:image'
+        content={`https://www.vectorowl.com/${image}`}
+      />
       <meta
         name='description'
         content='Explore a variety of free vector illustrations to enhance your website. Our vector art is available for download in WebP format. Premium illustrations available.'
